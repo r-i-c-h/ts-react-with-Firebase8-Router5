3b6248f7597292babffd5a9db577cddd10acd955
r-i-c-h <rich@richwerden.com>
2022-02-25 15:23:10 -0500

    Adds basic routing paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css'
+
+import Home from './pages/home/Home';
+import Recipe from './pages/recipe/Recipe';
+import Create from './pages/create/create';
+import Search from './pages/search/search';
 
 function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>It's an APP</h1>
-      </header>
-    </div>
-  )
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/recipes/:id">
+            <Recipe />
+          </Route>
+          <Route path="/search">
+            <Search />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>)
 }
 
 export default App

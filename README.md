a554e6480d2fe98a60c8f9fe9077458872993848
r-i-c-h <rich@richwerden.com>
2022-02-25 14:45:35 -0500

    init commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# CRUD App
+
+* Vite
+* TypeScript
+* React
+* React Router (v5)
+
+
++(local dummy data via `json-db`)
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>It's an APP</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App

f27a94c8b51d4d7f12791968867435d6f27a815f
r-i-c-h <rich@richwerden.com>
2022-02-25 20:03:48 -0500

    add: Animated CSS Loader

---
+++ src/components/LoaderAnimation.tsx
@@ -0,0 +1,11 @@
+import "./LoaderAnimation.scss";
+
+const LoaderAnimation = () => {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+    </div>
+  )
+}
+
+export default LoaderAnimation;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,13 +1,16 @@
+import LoaderAnimation from "../../components/LoaderAnimation";
 import useFetch from "../../hooks/useFetch";
 import { IRecipe } from "../../ts/interfaces";
 import "./Home.scss";
 
 const Home = () => {
   const { data, isPending, error } = useFetch<IRecipe>('http://localhost:3000/recipes')
+  console.log(data);
 
-  console.log("🚀 ~ file: Home.tsx ~ line 12 ~ Home ~ data, isPending, error", data, isPending, error)
   return (<div className="home">
     <h2>Home</h2>
+    {error && <p className="error">{error}</p>}
+    {isPending && <LoaderAnimation />}
   </div>)
 }
 

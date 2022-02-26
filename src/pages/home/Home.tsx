71b7e6fe94feead426c3c158feb50256ac2b8fe0
r-i-c-h <rich@richwerden.com>
2022-02-25 22:00:32 -0500

    feat:map <Home> useFetch data to <RecipeCard>s

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,17 +1,35 @@
-import LoaderAnimation from "../../components/LoaderAnimation";
-import useFetch from "../../hooks/useFetch";
 import { IRecipe } from "../../ts/interfaces";
+
+import useFetch from "../../hooks/useFetch";
+
+import LoaderAnimation from "../../components/LoaderAnimation";
+import RecipeCard from "../../components/RecipeCard";
+
 import "./Home.scss";
 
-const Home = () => {
-  const { data, isPending, error } = useFetch<IRecipe>('http://localhost:3000/recipes')
-  console.log(data);
 
-  return (<div className="home">
-    <h2>Home</h2>
-    {error && <p className="error">{error}</p>}
-    {isPending && <LoaderAnimation />}
-  </div>)
+const Home = () => {
+  const { data, isPending, error } = useFetch<IRecipe[]>('http://localhost:3000/recipes')
+
+  return (
+    <div className="home">
+      <h1 className="page-title">Home</h1>
+      {error && <p className="error">{error}</p>}
+      {isPending && <LoaderAnimation />}
+
+
+      {data &&
+        <div className="results">{data.map(
+          (eachRecipe: IRecipe) => {
+            return (
+              <RecipeCard key={eachRecipe.id} {...eachRecipe} />
+            )
+          }
+        )}
+        </div>}
+
+    </div>
+  )
 }
 
 export default Home;

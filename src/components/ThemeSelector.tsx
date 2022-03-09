5df193df4bb1e030a212adf1ca6328c7c7411f1c
r-i-c-h <rich@richwerden.com>
2022-03-09 16:36:41 -0500

    Starts dark-mode and ctx revision

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -1,14 +1,29 @@
 import { useTheme } from "../hooks/useTheme"
-
+import modeIcon from "../assets/mode-icon.svg"
 import './ThemeSelector.scss'
 
 const themeColors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme()
+  const { changeColor, mode, changeMode } = useTheme()
+
+  const toggleLightDarkThemes = () => {
+    const nextMode = mode === 'light' ? 'dark' : 'light'
+    changeMode(nextMode)
+  }
 
   return (
     <div className="theme-selector">
+
+      <div className="mode-toggle">
+        <img
+          src={modeIcon}
+          alt="Light\Dark Color Theme Switch Icon"
+          onClick={toggleLightDarkThemes}
+          style={{ filter: mode === "dark" ? 'invert(100%)' : 'invert(20%)' }}
+        />
+      </div>
+
       <div className="theme-buttons">
         {themeColors.map(color => (
           <div
--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,37 +1,58 @@
 import { createContext, useReducer } from "react";
 
-interface IThemeState {
-  color: string;
+interface IAction {
+  type: 'CHANGE_COLOR' | 'CHANGE_MODE';
+  payload?: string;
+}
+
+type ModeString = 'light' | 'dark'
+interface IState {
+  color: string,
+  mode: ModeString;
   changeColor: (color: string) => void;
-}
-interface IThemeActions {
-  type: string;
-  payload: string;
+  changeMode: (mode: string) => void;
 }
 
-const initialState: IThemeState = { color: '#58249c', changeColor: () => { null } };
+const initialState: IState = {
+  color: '#58249c',
+  mode: 'light',
+  changeColor: (color) => { },
+  changeMode: (mode) => { },
+  // I like the below approach more, but it doesn't work with the vite hmr?
+  // changeColor: (): void => { throw new Error('changeColor function must be overridden'); },
+  // changeMode: (): void => { throw new Error('changeMode function must be overridden'); },
+};
 
-export const ThemeContext = createContext<IThemeState>(initialState)
+export const ThemeContext = createContext<IState>(initialState) // No default per Kent C Dodds? how? d(>_<)b
 
-const themeReducer = (state: IThemeState, action: IThemeActions) => {
+function themeReducer(state: IState, action: IAction): IState {    //** REDUCER FUNCTION **//
+  // The action is an object with properties { type, payload }
   switch (action.type) {
     case 'CHANGE_COLOR':
-      return { ...state, color: action.payload }
+      return { ...state, color: action.payload as string }
+    case 'CHANGE_MODE':
+      return { ...state, mode: action.payload as ModeString }
     default:
       return state;
+    // throw new Error(`Unhandled action type: ${action.type}`)
   }
 }
 
 export const ThemeProvider: React.FC = ({ children }) => {
   const [state, dispatch] = useReducer(themeReducer, initialState);
-  const { color } = state;
+  //* Might* need to memoize this value - http://kcd.im/optimize-context
 
   const changeColor = (color: string) => {
     dispatch({ type: 'CHANGE_COLOR', payload: color })
   }
+  const changeMode = (mode: string) => {
+    dispatch({ type: 'CHANGE_MODE', payload: mode })
+  }
 
+  // const { color, mode } = state;
   return (
-    <ThemeContext.Provider value={{ color, changeColor }}>
+    <ThemeContext.Provider value={{ ...state, changeColor, changeMode }}>
       {children}
-    </ThemeContext.Provider>);
+    </ThemeContext.Provider>
+  );
 };

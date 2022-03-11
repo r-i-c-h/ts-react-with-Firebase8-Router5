24d1f505e706b15a152bfa90810dc7ea59bf4bc5
r-i-c-h <rich@richwerden.com>
2022-03-11 02:01:57 -0500

    Adds GET Recipes from Firebase in Home.tsx

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,16 +1,44 @@
+import { useEffect, useState } from "react";
+import { projectFirestore } from '../../firebase/config.js';
+
 import { IRecipe } from "../../ts/interfaces";
-
-import useFetch from "../../hooks/useFetch";
-
 import "./Home.scss";
 
 import LoaderAnimation from "../../components/LoaderAnimation";
 import RecipeList from "../../components/RecipeList";
 
 
-
 export default function Home() {
-  const { data, isPending, error } = useFetch<IRecipe[]>('http://localhost:3000/recipes')
+  const [data, setData] = useState<null | IRecipe[]>(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState<Error | boolean | string>(false);
+
+  useEffect(() => {
+    setIsPending(true);
+
+    projectFirestore.collection('recipes').get().then((snapshot) => {
+      if (snapshot.empty) {
+        setError('No Recipes Found in Database!')
+        setIsPending(false);
+      } else {
+        const results: IRecipe[] = [];
+
+        snapshot.docs.forEach(doc => {
+          const id = doc.id
+          const { method, title, ingredients, cookingTime } = doc.data();
+          results.push({ id, method, title, ingredients, cookingTime });
+        });
+        setData(results)
+        setIsPending(false);
+      }
+    }
+    ).catch(err => {
+      setError(err.message)
+      setIsPending(false);
+    })
+      .finally(() => setIsPending(false));
+  }, []);
+
   return (
     <div className="home">
       <h1 className="page-title">Home</h1>
